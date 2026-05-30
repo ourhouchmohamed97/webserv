8bd4920c55478e63c7379c4c704254376f6c9f1a
ourhouchmohamed97 <ourhouchmohamed97@gmail.com>
2026-05-30 14:15:04 +0100

    Feat: add AutoIndex dynamic html listing generator

---
+++ AutoIndex.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <dirent.h>
+
+class AutoIndex {
+public:
+    static std::string generate(const std::string& path, const std::string& dirPath) {
+        std::string html = "<html><body><h1>Index of " + dirPath + "</h1><hr><ul>";
+        DIR* dir = opendir(path.c_str());
+        if (dir == nullptr) return "";
+
+        struct dirent* entry;
+        while((entry = readdir(dir)) != nullptr) {
+            std::string name = entry->d_name;
+            if (name == ".") continue;
+            html += "<li><a href=\"" + name + "\">" + name + "</a></li>";
+        }
+        closedir(dir);
+        html += "</ul><hr></body></html>";
+        return html;
+    }
+};
